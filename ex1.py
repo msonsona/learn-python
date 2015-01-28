985649ebfac9e4d37a49782093f331c8876c0461
Miquel Sonsona <miquelsonsona@gmail.com>
2015-01-28 08:49:11 +0100

    printing just one line

--- ex1.py
+++ ex1.py
@@ -1,8 +1,9 @@
-print "Hello World!"
-print "Hello Again"
-print "I like typing this."
-print "This is fun."
-print 'Yay! Printing.'
-print "I'd much rather you 'not'."
-print 'I "said" do not touch this.'
-print 'Printing another line'
+#print "Hello World!"
+#print "Hello Again"
+#print "I like typing this."
+#print "This is fun."
+#print 'Yay! Printing.'
+#print "I'd much rather you 'not'."
+#print 'I "said" do not touch this.'
+#print 'Printing another line'
+print "Printing just this line"
