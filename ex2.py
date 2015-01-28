f01b93b802a32b51bf7515375ea551d537b15235
Miquel Sonsona <miquelsonsona@gmail.com>
2015-01-28 19:54:04 +0100

    exercise 2, comments

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# A comment, to increase readability of your code
+# Anything after the # is ignored by python
+
+print "I could have code like this." # and the comment after is ignored
+
+# You can use comments to "disable" a piece of code:
+# print "This won't run."
+
+print "This will run."
