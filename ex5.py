59422c081c7bea0fab083ece0dd9af8672159634
Miquel Sonsona <miquelsonsona@gmail.com>
2015-01-30 16:07:37 +0100

    exercise 5, unit changing

--- ex5.py
+++ ex5.py
@@ -5,10 +5,12 @@ weight = 75 # just guessing
 eyes = 'Brown'
 teeth = 'White'
 hair = 'Black'
+cm_per_inch = 2.54
+lbs_per_kilo = 2.205
 
 print "Let's talk about %s." % name
-print "He's %d centimeters tall." % height
-print "He's %d kilos heavy." % weight
+print "He's %d centimeters tall. That's %d inches." % (height, height / cm_per_inch)
+print "He's %d kilos heavy. That's %d pounds." % (weight, weight * lbs_per_kilo)
 print "Actually that's not too heavy."
 print "He's got %s eyes and %s hair." % (eyes, hair)
 print "His teeth are usually %s depending on the coffee." % teeth
