03a7a279e6f830fcd2df5a8a0f3f3ebc2e1e4c6a
Miquel Sonsona <miquelsonsona@gmail.com>
2015-01-30 15:58:52 +0100

    exercise 5, refactoring variable names

--- ex5.py
+++ ex5.py
@@ -1,17 +1,17 @@
-my_name = 'Miquel Sonsona'
-my_age = 30
-my_height = 180 # centimeters
-my_weight = 75 # just guessing
-my_eyes = 'Brown'
-my_teeth = 'White'
-my_hair = 'Black'
+name = 'Miquel Sonsona'
+age = 30
+height = 180 # centimeters
+weight = 75 # just guessing
+eyes = 'Brown'
+teeth = 'White'
+hair = 'Black'
 
-print "Let's talk about %s." % my_name
-print "He's %d centimeters tall." % my_height
-print "He's %d kilos heavy." % my_weight
+print "Let's talk about %s." % name
+print "He's %d centimeters tall." % height
+print "He's %d kilos heavy." % weight
 print "Actually that's not too heavy."
-print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
-print "His teeth are usually %s depending on the coffee." % my_teeth
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
 
 # this line is tricky, try to get it exactly right
-print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+print "If I add %d, %d, and %d I get %d." % (age, height, weight, age + height + weight)
