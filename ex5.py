fa67e6a6da4c29fe76749c102e234562195beed1
Miquel Sonsona <miquelsonsona@gmail.com>
2015-02-19 19:00:47 +0100

    testing %f formatter with custom precision

--- ex5.py
+++ ex5.py
@@ -9,10 +9,10 @@ cm_per_inch = 2.54
 lbs_per_kilo = 2.205
 
 print "Let's talk about %r." % name
-print "He's %d centimeters tall. That's %r inches." % (height, height / cm_per_inch)
+print "He's %d centimeters tall. That's %.2f inches." % (height, height / cm_per_inch)
 print "He's %d kilos heavy. That's %d pounds." % (weight, weight * lbs_per_kilo)
 print "Actually that's not too heavy."
-print "He's got %s eyes and %r hair." % (eyes, hair)
+print "He's got %s eyes and %s hair." % (eyes, hair)
 print "His teeth are usually %s depending on the coffee." % teeth
 
 # this line is tricky, try to get it exactly right
