615826b67cba929ab351deb89fca94f97577c4b8
Miquel Sonsona <miquelsonsona@gmail.com>
2015-02-24 20:23:15 +0100

    adding comments

--- ex15.py
+++ ex15.py
@@ -1,15 +1,24 @@
+# Importing argv from the sys module
 from sys import argv
 
+# Unpacking argument variables into script and filename vars
 script, filename = argv
 
+# Open the file named by the value in filename, and assign to variable txt
 txt = open(filename)
 
+# Print a string containing the filename passed as argument
 print "Here's your file %r:" % filename
+# Print the contents of the specified file, using read() function
 print txt.read()
 
+# Prompting the user
 print "Type the filename again:"
+# Read the input from the user
 file_again = raw_input("> ")
 
+# Open the file with the name the user gave
 txt_again = open(file_again)
 
+# Print the file's contents
 print txt_again.read()
