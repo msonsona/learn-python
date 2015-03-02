77579d7564ab6651896bdc360198c86a80e6e4e6
Miquel Sonsona <miquelsonsona@gmail.com>
2015-03-02 07:50:23 +0100

    functions and different ways to pass parameters

---
+++ ex19b.py
@@ -0,0 +1,30 @@
+def call_this_number(number):
+    print "Calling %d..." % number
+    print "Someone will answer shortly"
+
+def fake_number_generator():
+    return 42
+
+call_this_number(911)
+
+call_this_number(900 + 11)
+
+number_to_call = 911
+call_this_number(number_to_call)
+
+another_number = 900
+call_this_number(another_number + 11)
+
+catalan_emergency_number = 112
+call_this_number(catalan_emergency_number)
+
+call_this_number(number_to_call - catalan_emergency_number)
+
+call_this_number(catalan_emergency_number * 3)
+
+call_this_number(1 * number_to_call)
+
+custom_number = number_to_call / catalan_emergency_number
+call_this_number(custom_number)
+
+call_this_number(fake_number_generator())
