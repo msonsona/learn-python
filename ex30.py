b61fed3f320c4abdd122e933759cbf3b1ea76d35
Miquel Sonsona <miquelsonsona@gmail.com>
2015-03-11 15:01:04 +0100

    adding comments and other ifs

--- ex30.py
+++ ex30.py
@@ -1,22 +1,38 @@
-people = 30
+# Assign variables
+people = 50
 cars = 40
 trucks = 15
 
-if cars > people:
+# If there are more cars than people, or 50 fifty people
+if cars > people or people == 50:
     print "We should take the cars."
+# If there are less cars than people
 elif cars < people:
     print "We should not take the cars."
+# If there are as many cars as people (and not 50)
 else:
     print "We can't decide."
 
+# If there are more trucks than cars
 if trucks > cars:
     print "That's too many trucks."
+# If there are less trucks than cars
 elif trucks < cars:
     print "Maybe we could take the trucks."
+# finally, if there are as many trucks as cars
 else:
     print "We still can't decide."
 
+# If there are more people than trucks
 if people > trucks:
     print "Alright, let's just take the trucks."
+# If there are as many people as trucks or less
 else:
     print  "Fine, let's stay home then."
+
+# If there are exactly 50 people
+if people == 50:
+    print "There are 50 people!"
+# Otherwise
+else:
+    print "There aren't 50 people."
