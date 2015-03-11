4f28f22773f1baf2ebec41faef97affc3195ba3d
Miquel Sonsona <miquelsonsona@gmail.com>
2015-03-11 15:18:13 +0100

    adding more options to the game

--- ex31.py
+++ ex31.py
@@ -6,6 +6,7 @@ if door == "1":
     print "There's a giant bear here eating a cheese cake. What do you do?"
     print "1. Take the cake."
     print "2. Scream at the bear."
+    print "3. Dance with the bear."
     
     bear = raw_input("> ")
     
@@ -13,6 +14,8 @@ if door == "1":
         print "The bear eats your face off. Good job!"
     elif bear == "2":
         print "The bear eats your legs off. Good job!"
+    elif bear == "3":
+        print "The bear is so pleased that after a while, it just goes away."
     else:
         print "Well, doing %s is probably better. Bear runs away." % bear
 
@@ -29,5 +32,20 @@ elif door == "2":
     else:
         print "The insanity rots your eyes into a pool of muck. Good job!"
 
+elif door == "3":
+    print "Oh, you discovered a small, almost hidden door in the corner of the room."
+    print "Behind it, a small room contains a box. Do you open it?"
+    box = raw_input("> ")
+    
+    if box == "yes":
+        print "You find a grenade inside the box. Do you throw it?"
+        grenade = raw_input("> ")
+        
+        if grenade == "yes":
+            print "You explode into pieces, what else you thought would happen?"
+        else:
+            print "There is no way out, so you starve to death. Good job!"
+    else:
+        print "Oh, maybe the box was your only option. Good bye!"
 else:
     print "You stumble around and fall on a knife and die. Good job!"
