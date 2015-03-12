f9a06971624cfdc687a40802b4e9d91a3bd1e28b
Miquel Sonsona <miquelsonsona@gmail.com>
2015-03-12 08:37:12 +0100

    function to create list of numbers

--- ex33.py
+++ ex33.py
@@ -1,15 +1,22 @@
-i = 0
-numbers = []
-
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append(i)
+def my_range(number):
+    i = 0
+    numbers = []
     
-    i = i + 1
-    print "Numbers now: ", numbers
-    print "At the botton i is %d" % i
+    while i < number:
+        print "At the top i is %d" % i
+        numbers.append(i)
+        
+        i = i + 1
+        print "Numbers now: ", numbers
+        print "At the botton i is %d" % i
+    
+    print "The numbers: "
+    
+    for num in numbers:
+        print num
+    
+    return numbers
 
-print "The numbers: "
-
-for num in numbers:
-    print num
+numbers_1 = my_range(5)
+numbers_2 = my_range(10)
+numbers_3 = my_range(50)
