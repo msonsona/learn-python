2cfaac53b224ec24161cd19bd0800e6e97c08a69
Miquel Sonsona <miquelsonsona@gmail.com>
2015-03-12 15:16:34 +0100

    simplify int input

--- ex35.py
+++ ex35.py
@@ -3,11 +3,7 @@ from sys import exit
 def gold_room():
     print "This room is full of gold. How much do you take?"
     
-    choice = raw_input("> ")
-    if "0" in choice or "1" in choice:
-        how_much = int(choice)
-    else:
-        dead("Man, learn to type a number.")
+    how_much = int(raw_input("> "))
     
     if how_much < 50:
         print "Nice, you're not greedy, you win!"
